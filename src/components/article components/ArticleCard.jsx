85c02bddae024c0e9531cebb0e827671c0ee73c2
Wanb00 <wansenglok6@gmail.com>
2025-05-23 16:23:44 +0100

    added article images

--- src/components/article components/ArticleCard.jsx
+++ src/components/article components/ArticleCard.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article }) => {
     const { title, topic, author, created_at, votes } = article;
-
+    console.log(article)
     return (
         <div className="article-card">
             <Link to={`/articles/${article.article_id}`} className="article-link">
                 <h2>{title}</h2>
             </Link>
+                <img src={article.article_img_url} className="article-img" alt={`article image for ${title}`}></img>
                 <p><strong>Topic:</strong> {topic}</p>
                 <p><strong>Author:</strong> {author}</p>
                 <p><strong>Date:</strong> {new Date(created_at).toLocaleDateString()}</p>
