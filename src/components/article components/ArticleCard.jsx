025e3f548bbebb8b1925b8143c36f30b97cd7a31
Wanb00 <wansenglok6@gmail.com>
2025-06-16 11:37:46 +0100

    added phone responsiveness and refactored all articles page

--- src/components/article components/ArticleCard.jsx
+++ src/components/article components/ArticleCard.jsx
@@ -2,17 +2,18 @@ import { Link } from "react-router-dom";
 
 const ArticleCard = ({ article }) => {
     const { title, topic, author, created_at, votes } = article;
-    console.log(article)
+    const titleArr = title.split(" ");
+    const splicedTitle = titleArr.length > 5 ? titleArr.splice(0, 5).join(" ") + "..." : title;
     return (
         <div className="article-card">
             <Link to={`/articles/${article.article_id}`} className="article-link">
-                <h2>{title}</h2>
+                <h2>{splicedTitle}</h2>
             </Link>
                 <img src={article.article_img_url} className="article-img" alt={`article image for ${title}`}></img>
-                <p><strong>Topic:</strong> {topic}</p>
-                <p><strong>Author:</strong> {author}</p>
-                <p><strong>Date:</strong> {new Date(created_at).toLocaleDateString()}</p>
-                <p><strong>Votes:</strong> {votes}</p>
+                <p className="tags"><strong>Topic:</strong> {topic}</p>
+                <p className="tags"><strong>Author:</strong> {author}</p>
+                <p className="tags mobile-hidden"><strong>Date:</strong> {new Date(created_at).toLocaleDateString()}</p>
+                <p className="tags mobile-hidden"><strong>Votes:</strong> {votes}</p>
         </div>
     );
 };
