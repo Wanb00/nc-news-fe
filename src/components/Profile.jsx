6049f2b35edfd4833ae8efccbbb718427502e288
Wanb00 <wansenglok6@gmail.com>
2025-06-25 15:49:38 +0100

    user local storage added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -18,7 +18,7 @@ const Profile = () => {
       }
     };
     fetchArticles();
-  }, []);
+  }, [loggedInUser]);
 
   return (
     <section>
--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,9 +1,24 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
     const [loggedInUser, setLoggedInUser] = useState(null);
+
+    useEffect(() => {
+        const savedUser = localStorage.getItem("loggedInUser");
+        if (savedUser) {
+            setLoggedInUser(JSON.parse(savedUser));
+        }
+    }, [])
+
+    useEffect(() => {
+        if (loggedInUser) {
+            localStorage.setItem("loggedInUser", JSON.stringify(loggedInUser));
+        } else {
+            localStorage.removeItem("loggedInUser");
+        }
+    }, [loggedInUser])
 
     return (
         <UserContext.Provider value={{ loggedInUser, setLoggedInUser }}>
